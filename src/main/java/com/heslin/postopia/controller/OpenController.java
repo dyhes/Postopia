cb9255760ebf30d3f30266199b8752f2df866005
dyes <1325574784@qq.com>
2024-07-11 11:09:08 +0800

    refactor: use record for dto objects

---
+++ src/main/java/com/heslin/postopia/controller/OpenController.java
@@ -0,0 +1,9 @@
+package com.heslin.postopia.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/open")
+public class OpenController {
+}
