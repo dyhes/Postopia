a69d2ebf9c4c31923445178925bc49c87422191e
dyhes <1325574784@qq.com>
2024-07-12 15:08:45 +0800

    refactor: mark space in post model as FetchType.LAZY

--- src/main/java/com/heslin/postopia/model/Post.java
+++ src/main/java/com/heslin/postopia/model/Post.java
@@ -8,6 +8,7 @@ import org.springframework.data.annotation.CreatedDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -35,7 +36,7 @@ public class Post {
     @JoinColumn(name = "poster_id")
     private User poster;
 
-    @ManyToOne
+    @ManyToOne(fetch=FetchType.LAZY)
     @JoinColumn(name = "space_id")
     private Space space;
     
