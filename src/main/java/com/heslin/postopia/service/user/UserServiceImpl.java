1591348c283b6b32d78c4273d38eb46c6c8c139e
dyes <1325574784@qq.com>
2024-07-13 23:51:18 +0800

    fix: add @Service to userserviceimpl

--- src/main/java/com/heslin/postopia/service/user/UserServiceImpl.java
+++ src/main/java/com/heslin/postopia/service/user/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package com.heslin.postopia.service.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.heslin.postopia.model.User;
 import com.heslin.postopia.repository.UserRepository;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
