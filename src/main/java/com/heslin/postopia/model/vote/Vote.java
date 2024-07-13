fefd264945558869175a83c5c5d028b23a47dda4
dyhes <1325574784@qq.com>
2024-07-13 22:20:06 +0800

    fix: fix polymophic query error

--- src/main/java/com/heslin/postopia/model/vote/Vote.java
+++ src/main/java/com/heslin/postopia/model/vote/Vote.java
@@ -6,16 +6,23 @@ import java.util.Set;
 
 import com.heslin.postopia.model.opinion.VoteOpinion;
 
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
-@MappedSuperclass
-public class Vote {
+@Entity
+@Table(name = "votes")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "vote_type")
+public abstract class Vote {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
