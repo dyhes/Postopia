f32de2a4a532655865f6c91bc227b4516ecc190f
dyes <1325574784@qq.com>
2024-07-13 22:20:50 +0800

    fix: fix value error

--- src/main/java/com/heslin/postopia/service/jwt/JWTService.java
+++ src/main/java/com/heslin/postopia/service/jwt/JWTService.java
@@ -17,16 +17,16 @@ import io.jsonwebtoken.security.Keys;
 @Service
 public class JWTService {
 
-    @Value("${ postopia.jwt.secret}")
+    @Value("${postopia.jwt.secret}")
     private String secret;
 
-    @Value("${ postopia.jwt.expiration}")
+    @Value("${postopia.jwt.expiration}")
     private Long jwtExpiration;
 
-    @Value("${ postopia.jwt.refresh-token.expiration}")
+    @Value("${postopia.jwt.refresh-token.expiration}")
     private Long refreshExpiration;
 
-    @Value("${ postopia.jwt.issuer}")
+    @Value("${postopia.jwt.issuer}")
     private String issuer;
 
     private SecretKey getSigningKey() {
