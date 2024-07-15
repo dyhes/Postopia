93573c54f9d83a5dc875e919e9cd24004edb7f07
dyhes <1325574784@qq.com>
2024-07-15 19:25:05 +0800

    fix: return space id after creating space

--- src/main/java/com/heslin/postopia/service/space/SpaceService.java
+++ src/main/java/com/heslin/postopia/service/space/SpaceService.java
@@ -7,6 +7,7 @@ import com.heslin.postopia.dto.Message;
 import com.heslin.postopia.dto.SpaceInfo;
 import com.heslin.postopia.enums.PopularSpaceOrder;
 import com.heslin.postopia.model.User;
+import com.heslin.postopia.util.Pair;
 
 public interface SpaceService {
 
@@ -14,7 +15,7 @@ public interface SpaceService {
     
     public Message leaveSpace(Long spaceId, User user);
 
-    public Message createSpace(User user, String name, String description, String avatar);
+    public Pair<Message, Long> createSpace(User user, String name, String description, String avatar);
 
     public Page<SpaceInfo> getSpacesByUserId(Long userId, Pageable pageable);
 
