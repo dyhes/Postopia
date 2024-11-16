433e045451654fce2022d14dba31767c83cba909
heslin <73648326+dyhes@users.noreply.github.com>
2024-11-16 17:20:31 +0800

    feat: impelent simple reply query

--- src/main/java/com/heslin/postopia/model/Comment.java
+++ src/main/java/com/heslin/postopia/model/Comment.java
@@ -1,29 +1,17 @@
 package com.heslin.postopia.model;
 
+import com.heslin.postopia.model.opinion.CommentOpinion;
+import com.heslin.postopia.model.vote.CommentVote;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import com.heslin.postopia.model.opinion.CommentOpinion;
-import com.heslin.postopia.model.vote.CommentVote;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import lombok.Data;
 
 @Data
 @Entity
@@ -58,7 +46,7 @@ public class Comment {
     @JoinColumn(name = "parent_id")
     private Comment parent;
 
-    @OneToMany(mappedBy = "parent", orphanRemoval = true)
+    @OneToMany(mappedBy = "parent", orphanRemoval = true, fetch = FetchType.EAGER)
     private List<Comment> children = new ArrayList<>();
 
     @CreatedDate
