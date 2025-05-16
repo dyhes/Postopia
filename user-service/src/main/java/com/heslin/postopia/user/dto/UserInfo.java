4bddfcbf7e2dc0fffb8aadeddbdd8cc2396bbbd9
dyhes <1325574784@qq.com>
2025-05-16 14:02:07 +0800

    fix: UserInfo Error

--- user-service/src/main/java/com/heslin/postopia/user/dto/UserInfo.java
+++ user-service/src/main/java/com/heslin/postopia/user/dto/UserInfo.java
@@ -5,10 +5,4 @@ import com.heslin.postopia.common.dto.UserId;
 import java.time.Instant;
 
 public record UserInfo(UserId userId, String username, String nickname, String avatar, Long postCount, Long commentCount, Long credit, String introduction, String email, boolean showEmail, Instant createdAt) {
-
-    public UserInfo {
-        if (!showEmail) {
-            email = "保密";
-        }
-    }
 }
