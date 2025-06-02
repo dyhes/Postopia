c5ffa142ad80e9580dfdcbc73887a25a13f2eac0
dyhes <1325574784@qq.com>
2025-06-02 14:49:51 +0800

    fix: userId in message

--- common/src/main/java/com/heslin/postopia/common/utils/PostopiaFormatter.java
+++ common/src/main/java/com/heslin/postopia/common/utils/PostopiaFormatter.java
@@ -1,4 +1,5 @@
 package com.heslin.postopia.common.utils;
+import com.heslin.postopia.common.dto.UserId;
 import com.heslin.postopia.common.exception.BadRequestException;
 
 public class PostopiaFormatter {
@@ -9,15 +10,15 @@ public class PostopiaFormatter {
     }
 
     public static String formatUser(Long userId, String username) {
-        return String.format(" 用户 postopia-user{%d;@%s} ", userId, username);
+        return String.format("用户 postopia-user{%d;@%s} ", UserId.masked(userId), username);
     }
 
     public static String formatSpace(Long spaceId, String spaceName) {
-        return String.format(" 空间 postopia-space{%d;%s} ", spaceId, spaceName);
+        return String.format("空间 postopia-space{%d;%s} ", spaceId, spaceName);
     }
 
     public static String formatPost(Long spaceId, Long postId, String content) {
-        return String.format(" postopia-post{%s;%d;%s} ", spaceId, postId, content);
+        return String.format(" postopia-post{%d;%d;%s} ", spaceId, postId, content);
     }
 
     public static String formatPost(Long spaceId, Long postId) {
@@ -25,7 +26,7 @@ public class PostopiaFormatter {
     }
 
     public static String formatComment(Long spaceId, Long postId, Long commentId, String content) {
-        return String.format(" postopia-comment{%s;%d;%d;%s} ", spaceId, postId, commentId, content);
+        return String.format(" postopia-comment{%d;%d;%d;%s} ", spaceId, postId, commentId, content);
     }
 
     public static String formatComment(Long spaceId, Long postId, Long commentId) {
